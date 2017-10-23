d9baee00e1981f5b328dff673ffbd96e39dc9fa0
Sylwester Guzek <Sylwester.guzek@wp.pl>
2017-10-23 15:14:48 +0200

    removed frontend files

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,15 @@ var Tajemnica = require('../models/tajemnica');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if (process.env.NODE_ENV == 'prod') {
-		console.log('production mode with separate react front end app');
-		return res.redirect('/');
-	}
-	else {
-		console.log('development mode with integrated server-rendered front end');
-		var messages = req.flash('error');
+	// if (process.env.NODE_ENV == 'prod') {
+	// 	console.log('production mode with separate react front end app');
+	// 	return res.redirect('/');
+	// }
+	// else {
+	// 	console.log('development mode with integrated server-rendered front end');
+	 	var messages = req.flash('error');
 		return res.render('index', { messages:messages, hasErrors: messages.length>0});
-	}
+	// }
 });
 
 
